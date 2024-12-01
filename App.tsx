f7cd11f74ff6cee2e8da40b0c2ddb8c5ac5330e9
ddalynng <d.fangloy@gmail.com>
2024-12-01 19:20:00 +0100

    supabase funtion added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import supabase from "./src/lib/supabase";
 
 export default function App() {
+  const [query, setQuery] = useState("");
+
+  const runPrompt = async () => {
+    const { data, error } = await supabase.functions.invoke("prompt", {
+      body: { query, name: "dalin" },
+    });
+
+    if (error) {
+      console.log(error);
+    }
+    console.log(data);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        placeholder="prompt"
+        value={query}
+        onChangeText={setQuery}
+        style={{
+          padding: 10,
+          borderColor: "gainsboro",
+          borderWidth: 1,
+          borderRadius: 5,
+        }}
+      />
+      <Button title="Run" onPress={runPrompt} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    padding: 10,
   },
 });
